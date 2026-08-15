752fd298be6d8b752a63a92a5f4f38f4bb06143a
Soham Das <61263629+Sodas0@users.noreply.github.com>
2026-08-15 10:57:12 -0700

    Add: subsets - submission-7

---
+++ Data Structures & Algorithms/subsets/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        out = []
+        
+        def dfs(start, path):
+            # collect every node
+            out.append(path.copy())
+
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                dfs(i+1, path)
+                path.pop()
+        
+        dfs(start=0, path=[])
+        return out
